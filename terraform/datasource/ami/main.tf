579dc232be141164d1566765ffa5ea33977a207c
GopiKrishna <padyalagopikrishna@gmail.com>
2023-12-20 20:22:14 +0530

    added ami file

---
+++ terraform/datasource/ami/main.tf
@@ -0,0 +1,43 @@
+data "aws_ami" "example" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "web-server" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
